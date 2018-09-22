de2c908100178a19036f00f583bd63418cb234f9
Vinicius Marques <vinicius.marques@clickqi.com.br>
2018-09-22 16:15:30 -0300

    fix(login): adjusts layout page

--- Aplicação/back/public/apidoc/api_project.js
+++ Aplicação/back/public/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-09-21T02:04:14.619Z",
+    "time": "2018-09-22T18:13:24.426Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
